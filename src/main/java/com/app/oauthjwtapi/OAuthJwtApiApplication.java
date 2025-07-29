004f20cf919883d5fbf0b8ee6508c39104f93d87
SofiaBritps <sofiibritos01@gmail.com>
2025-07-29 20:54:56 -0300

    feat: initial Configuration

---
+++ src/main/java/com/app/oauthjwtapi/OAuthJwtApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.oauthjwtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthJwtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthJwtApiApplication.class, args);
+    }
+
+}
